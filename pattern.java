c66ebbb888b5ac8d948571bfb38c7bfecf893eb4
Hrishi Raj Verma <vhraj1629@gmail.com>
2025-01-24 22:10:33 +0530

    change(;)

--- pattern.java
+++ pattern.java
@@ -15,6 +15,7 @@ import java.util.*;
 public class pattern {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt()
+        int a = sc.nextInt();
+
     }
 }
