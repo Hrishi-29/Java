fbc18b9b3f34e6b5d94e4379e077fe002fd71630
Hrishi Raj Verma <vhraj1629@gmail.com>
2026-02-11 11:08:08 +0530

    input elements in matrix

---
+++ diagonalSum/arrayCode.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class arrayCode {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        System.out.print("Enter a size of matrix: ");
+        int[][] arr = new int[size][size];
+        System.out.print("Enter elements: ");
+        for (int i = 0; i<arr.length; i++) {
+            for (int j = 0; j<arr[0].length; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        sc.close();
+    }
+}
