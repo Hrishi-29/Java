ed5dcef7824924e07cc782aeaa1d5c357980796b
Hrishi Raj Verma <vhraj1629@gmail.com>
2026-02-11 12:10:30 +0530

    small mistake

--- diagonalSum/arrayCode.java
+++ diagonalSum/arrayCode.java
@@ -2,8 +2,8 @@ import java.util.*;
 public class arrayCode {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int size = sc.nextInt();
         System.out.print("Enter a size of matrix: ");
+        int size = sc.nextInt();
         int[][] arr = new int[size][size];
         System.out.print("Enter elements: ");
         for (int i = 0; i<arr.length; i++) {
