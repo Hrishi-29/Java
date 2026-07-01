7ca0218a82d16784abc7af624468e4d49ae78d70
Hrishi Raj Verma <vhraj1629@gmail.com>
2026-07-01 17:40:09 +0530

    traversing an array

--- diagonalSum/arrayCode.java
+++ diagonalSum/arrayCode.java
@@ -12,5 +12,11 @@ public class arrayCode {
             }
         }
         sc.close();
+        for (int i= 0; i<arr.length; i++) {
+            for (int j= 0; j<arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
