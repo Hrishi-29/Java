408b9463756a1882e10b85567ff7670a7a9208ac
Hrishi Raj Verma <vhraj1629@gmail.com>
2026-07-08 14:55:09 +0530

    bubbleSort Qs get a error

---
+++ sortingAlgorithms/bubbleSort.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class bubbleSort {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a size of array: ");
+		int size = sc.nextInt();
+		int[] arr = new int[size];
+		System.out.print("Enter an element of array: ");
+		for (int i= 0; i<arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+		sc.close();
+		System.out.println("Entered elements: "+ Arrays.toString(arr));
+		System.out.println("In Descending Order: "+ Arrays.toString(bubbleSorting(arr)));
+	}
+	public static int[] bubbleSorting(int arr[]) {
+		boolean swapped = false;
+		for (int i = 0; i<arr.length; i++) {
+			if (!swapped) {
+				for (int j=0; j<arr.length-1; j++) {
+					if (arr[i]<arr[j+1]) {
+						int temp = arr[i];
+						arr[i] = arr[j+1];
+						arr[j+1] = temp;
+						swapped = true;
+					}
+				}
+			}
+		}
+		if (swapped) {
+			return arr;
+		}
+	}
+}
