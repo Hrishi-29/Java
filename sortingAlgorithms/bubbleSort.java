dae64b050ba3d66cf4e54a95354c116a099f6001
Hrishi Raj Verma <vhraj1629@gmail.com>
2026-07-09 16:01:18 +0530

    errors fixed

--- sortingAlgorithms/bubbleSort.java
+++ sortingAlgorithms/bubbleSort.java
@@ -15,18 +15,19 @@ public class bubbleSort {
 	}
 	public static int[] bubbleSorting(int arr[]) {
 		boolean swapped = false;
-		for (int i = 0; i<arr.length; i++) {
-			for (int j=0; j<arr.length-1; j++) {
-				if (arr[i]<arr[j+1]) {
-					int temp = arr[i];
-					arr[i] = arr[j+1];
-					arr[j+1] = temp;
-					swapped = true;
+			for (int i = 0; i<arr.length; i++) {
+				for (int j = 0; j<arr.length-i-1; j++) {
+					if (arr[j]<arr[j+1]) {
+						int temp = arr[j];
+						arr[j] = arr[j+1];
+						arr[j+1] = temp;
+						swapped = true;
+					}
+				}
+				if (!swapped) {
+					break;
 				}
 			}
-			if (!swapped) {
-				return arr;
-			}
-		}
+		return arr;
 	}
 }
