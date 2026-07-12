6c3f09cbc9bcb5e3af19bc0bb2518eda09f1a36d
Hrishi Raj Verma <vhraj1629@gmail.com>
2026-07-12 16:04:22 +0530

    selectionSort Qs

---
+++ sortingAlgorithms/selectionSort.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class selectionSort {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a size of array: ");
+		int size = sc.nextInt();
+		int[] arr = new int[size];
+		System.out.print("Enter an elements of array: ");
+		for (int i=0; i<arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+		sc.close();
+		System.out.println("Entered Array: "+ Arrays.toString(arr));
+		System.out.println("Sorted Array: "+ Arrays.toString(selectionSort(arr)));
+	}
+	public static int[] selectionSort(int arr[]) {
+		for (int i=0; i<arr.length; i++) {
+		 	int largest = i;
+		 	for (int j=0; j<arr.length-i-1; j++) {
+		 		if (arr[i]<arr[largest]) {
+		 		 	largest=j;																	
+		 		}       
+		 	}
+
+		 	int temp = arr[largest];
+		 	arr[largest] = arr[i];
+		 	arr[i] = temp; 	                                            
+		}
+		return arr;
+	}
+}
