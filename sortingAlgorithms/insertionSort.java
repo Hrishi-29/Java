d269484ff8bc98a7b61acbb50ffc44706e1c4d5d
Hrishi Raj Verma <vhraj1629@gmail.com>
2026-07-21 13:54:07 +0530

    output in decreasing array

--- sortingAlgorithms/insertionSort.java
+++ sortingAlgorithms/insertionSort.java
@@ -16,7 +16,7 @@ public class insertionSort {
 		for (int i = 1; i<arr.length; i++) {
 			int key = arr[i];
 			int j = i - 1;
-			while (j>=0 && arr[j]>key) {
+			while (j>=0 && arr[j]<key) {
 				arr[j+1] = arr[j];
 				j-=1;
 			}
